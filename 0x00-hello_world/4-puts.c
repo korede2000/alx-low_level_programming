ed46dd2c4cab2911236c4344c55090bfa89ebc91
Molade Abdul Fattah <moladefattah@gmail.com>
2022-09-08 16:21:47 -0700

    add puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 
-/*
+/**
  * main - Entry point
  *
  * description: prints "Programming is like building a multilingual puzzle"
  *
  * Return: Always 0
  */
-int main (void)
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
