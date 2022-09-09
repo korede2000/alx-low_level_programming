a3a86db4ac59c5484f7b2927c54cf062f88a661b
Molade Abdul Fattah <moladefattah@gmail.com>
2022-09-09 10:19:46 -0700

    print lowercase

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - print lowercase characers from a-z
+ *
+ * Return: 0
+ *
+ */
+int main(void)
+{
+	char ch = 'a';
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		putchar(ch);
+		putchar('\n');
+	}
+	return (0);
+}
