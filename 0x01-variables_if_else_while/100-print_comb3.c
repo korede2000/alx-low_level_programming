55d82cdacdff42405c19375dab5e3d3f629c637b
Molade Abdul Fattah <moladefattah@gmail.com>
2022-09-09 16:13:50 -0700

    double digit combo

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+/**
+ * main - prints all possible different combinations of two digits
+ *
+ * Return: 0
+ *
+ */
+int main(void)
+{
+	int i = 48;
+	int j = 48;
+
+
+	while (i < 58)
+	{
+		j = i + 1;
+		while (j < 58)
+		{
+			putchar(i);
+			putchar(j);
+
+			if (i < 56 || j < 57)
+			{
+				putchar(44);
+				putchar(32);
+			}
+			j++;
+		}
+		i++;
+	}
+	putchar(10);
+	return (0);
+}
