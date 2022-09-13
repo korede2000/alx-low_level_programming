fcf8d7f66175c4189efa9d6d7cdb4fcad4d7aa6e
Molade Abdul Fattah <moladefattah@gmail.com>
2022-09-13 08:18:32 -0700

    0-putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - program that prints _putchar, then a new line
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	char str[8] = "_putchar";
+	int i = 0;
+
+	while (i < 8)
+	{
+		putchar(str[i]);
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
