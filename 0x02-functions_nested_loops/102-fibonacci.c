d005a2f46072770972df3708de4afa07c0c49955
Molade Abdul Fattah <moladefattah@gmail.com>
2022-09-14 02:39:41 -0700

    fibonnaci

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+/**
+ * main - print first 50 fibonacci numbers
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i, current, previous;
+	unsigned long fibb;
+
+	i = 0;
+	previous = 1;
+	current = 2;
+	while (i < 50)
+	{
+		if (i == 0)
+		{
+			printf("%d, ", previous);
+		}
+		else if (i == 1)
+		{
+			printf("%d, ", current);
+		}
+		else
+		{
+			fibb = current + previous;
+			if( i < 49)
+				printf("%lu, ", fibb);
+			else
+				printf("%lu", fibb);
+			previous = current;
+			current = fibb;
+		}
+		i++;
+	}
+}
