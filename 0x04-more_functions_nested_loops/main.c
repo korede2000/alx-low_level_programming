34401915e4e3335b67d23547b371cfa31668e3a1
Molade Abdul Fattah <moladefattah@gmail.com>
2022-09-16 12:10:57 -0700

    print num

--- 0x04-more_functions_nested_loops/main.c
+++ 0x04-more_functions_nested_loops/main.c
@@ -9,9 +9,15 @@
 
 int main(void)
 {
-	print_triangle(2);
-	print_triangle(10);
-	print_triangle(1);
-	print_triangle(0);
+	print_number(98);
+	_putchar('\n');
+	print_number(402);
+	_putchar(10);
+	print_number(1024);
+	_putchar(10);
+	print_number(0);
+	_putchar(10);
+	print_number(-98);
+	_putchar(10);
 	return (0);
 }
