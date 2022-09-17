42fcf1ab30baf489dcd12701eda4cfa0f7f8ba00
Molade Abdul Fattah <moladefattah@gmail.com>
2022-09-17 05:10:37 -0700

    fibonacci sum

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+/**
+ * main - sum of fibonacci series up to 4000000
+ * that is even.
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+	long int current, previous, fibb;
+	long long int sum;
+
+	i = 0;
+	previous = 0;
+	current = 1;
+	fibb = 0;
+	sum = 0;
+
+	while (fibb < 4000000)
+	{
+		fibb = previous + current;
+		if (fibb % 2 == 0)
+			sum = sum + fibb;
+		previous = current;
+		current = fibb;
+		i++;
+	}
+	printf("%lld\n", sum);
+
+	return (0);
+}
