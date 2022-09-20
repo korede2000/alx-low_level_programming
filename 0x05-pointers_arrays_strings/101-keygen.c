f19e71cae59a953d8159682fdc991ba93e5192c4
Molade Abdul Fattah <moladefattah@gmail.com>
2022-09-20 11:42:39 -0700

    keygen

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -10,17 +10,22 @@
 
 int main(void)
 {
-	char c;
-	int x;
-
-	srand(time(0));
-	while (x <= 2645)
-	{
-		c = rand() % 128;
-		x += c;
-		putchar(c);
-	}
-	putchar(2772 - x);
-
-	return (0);
-	}
+int i, sum, n;
+int pass[100];
+sum = 0;
+srand(time(NULL));
+for (i = 0; i < 100; i++)
+{
+pass[i] = rand() % 78;
+sum += (pass[i] + '0');
+putchar(pass[i] + '0');
+if ((2772 - sum) - '0' < 78)
+{
+n = 2772 - sum - '0';
+sum += n;
+putchar(n + '0');
+break;
+}
+}
+return (0);
+}
