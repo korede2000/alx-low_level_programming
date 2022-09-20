c193ca60ee4e31a733dbd74f38353f7349e90b11
Molade Abdul Fattah <moladefattah@gmail.com>
2022-09-20 11:19:26 -0700

    atoi

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _atoi - convert a string to integer
+ * @s: - string
+ * Return: integer
+ */
+
+int _atoi(char *s)
+{
+	int i, num;
+
+	i = 1;
+	num = 0;
+
+	do {
+		if (*s == '-')
+			i *= -1;
+		else if (*s >= '0' && *s <= '9')
+			num = num * 10 + (*s - '0');
+		else if (num > 0)
+			break;
+	} while (*s++);
+	return (num * i);
+}
