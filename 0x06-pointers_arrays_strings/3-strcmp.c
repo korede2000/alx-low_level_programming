8c34593cce38a7f51756b4dcd7814a8c23198cc5
Molade Abdul Fattah <moladefattah@gmail.com>
2022-09-21 11:47:56 -0700

    _strcmp

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -12,16 +12,31 @@
 
 int _strcmp(char *s1, char *s2)
 {
-	int diff;
+	int diff = 0, i = 0;
 
-	while (*s1)
+	while(1)
 	{
-		if (*s1 != *s2)
+		if (s1[i] == '\0' && s2[i] == '\0')
 			break;
-		s1++;
-		s2++;
+		else if (s1[i] == '\0')
+		{
+			diff = s2[i];
+			break;
+		}
+		else if (s2[i] == '\0')
+		{
+			diff = s1[i];
+			break;
+		}
+		else if (s1[i] != s2[i])
+		{
+			diff = s1[i] - s2[i];
+			break;
+		}
+		else
+			i++;
+
 	}
 
-	diff = s1 - s2;
 	return (diff);
 }
