8ea34c7f4e6db222ecda95b14d587a2c7a841941
Molade Abdul Fattah <moladefattah@gmail.com>
2022-10-10 09:06:06 -0700

    deefine a struct

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - define a new type of data dog
+ * @name: name of dog
+ * @age: age of the dog
+ * @owner: dog owner
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+
+#endif
