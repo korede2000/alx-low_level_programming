37212e19e7e4ad6bfb3ccfc706c42965223f25f4
Molade Abdul Fattah <moladefattah@gmail.com>
2022-10-10 09:29:38 -0700

    initialize dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,8 @@
 #ifndef DOG_H
 #define DOG_H
 
+#include <stdio.h>
+#include <stdlib.h>
 /**
  * struct dog - define a new type of data dog
  * @name: name of dog
@@ -14,5 +16,7 @@ struct dog
 	float age;
 	char *owner;
 };
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
 
 #endif
