3a9c606e345f0a6a54fd3263bbf2cd881ffa5150
Molade Abdul Fattah <moladefattah@gmail.com>
2022-10-13 10:31:21 -0700

    variadic func

--- 0x10-variadic_functions/1-sm.c
+++ 0x10-variadic_functions/1-sm.c
@@ -10,16 +10,15 @@
 
 int sum_them_all(const unsigned int n, ...)
 {
+	if (n == 0)
+		return (0);
+
 	va_list ap;
 	unsigned int i;
 	int sum;
 
 	va_start(ap, n);
 	sum = 0;
-
-	if (n == 0)
-		return (0);
-
 	for (i = 0; i < n; i++)
 		sum += va_arg(ap, unsigned int);
 
